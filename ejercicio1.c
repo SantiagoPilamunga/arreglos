5b6e1463aa21c71f07290505e45781338029c7de
Saniago <saniago.pilamunga@udla.edu.ec>
2023-11-07 14:26:36 -0500

    cambios

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main(int argc, char const *argv[])
 {
+    srand (time(NULL));
     int vector1[10];
-    for (int i = 0; i <= 10; i++)
+    printf("Ingrese 10 numeros \n");
+    for (int i = 0; i <10; i++)
     {
-        
+        //scanf("%d", &vector1[i]);
+        vector1[i]=rand()%100+1;
     }
-    
+    for (int i = 0; i <10; i++)
+    {
+        printf("%d \n", vector1[i]);
+    }
 
 
     return 0;
